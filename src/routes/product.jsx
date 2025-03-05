39611822975782eaefb72fd5c4585455857cbe5e
fillip husebø <felipehusebo@gmail.com>
2025-03-05 21:10:01 +0100

    display data from fetch

--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getSpecificProduct } from "../api/getSpecificProduct.jsx";
-
 
 export function RenderProduct() {
     const { id } = useParams();
@@ -12,19 +11,24 @@ export function RenderProduct() {
             try {
                 const data = await getSpecificProduct(id);
                 setProduct(data);
-                console.log(product)
             } catch (error) {
                 console.error("Error fetching product:", error);
             }
-
         };
         fetchProduct();
     }, [id]);
 
+    if (!product) {
+        return <p>Loading...</p>;
+    }
 
     return (
-        <>
-
-        </>
+        <div>
+            <h1>{product.title}</h1>
+            <img src={product.image.url} alt=""/>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            <Link to="/">Back to Products</Link>
+        </div>
     );
 }
