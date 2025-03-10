858445364a4cfba5e1592e3af2d3ded585e4e9ba
fillip husebø <felipehusebo@gmail.com>
2025-03-10 18:20:04 +0100

    added look ahead search bar that takes user to product page when clicked

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
-import {useCart} from "../../store.js";
+import { useCart } from "../../store.js";
+import {SearchBar} from "../Buttons/SearchBar.jsx";
 
 export function Header() {
     const { cart } = useCart();
     const itemCount = cart.reduce((total, item) => total + item.quantity, 0);
 
     return (
-        <header className="flex gap-10 font-bold">
+        <header className="flex gap-10 font-bold items-center">
             <Link to="">Home</Link>
             <Link to="/contact">Contact</Link>
+            <SearchBar />
             <Link to="/cart">
                 Cart {itemCount > 0 ? `(${itemCount})` : ""}
             </Link>
